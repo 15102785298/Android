87a44ff52cec377f75ed0ed68034341a5ba0c219
15102785298 <517576151@qq.com>
2016-12-27 17:59:06 +0800

    ---V1.0.1

--- Haier_Machine/src/wust/dayin1/activity/BindMachineActivity.java
+++ Haier_Machine/src/wust/dayin1/activity/BindMachineActivity.java
@@ -4,13 +4,21 @@ import com.example.haier_machine.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class BindMachineActivity extends Activity{
+public class BindMachineActivity extends Activity implements OnClickListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_bind_machine);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
 	}
 }
