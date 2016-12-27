7bb3db6fc8f74ee433c024e6a7daf491d90ded25
15102785298 <517576151@qq.com>
2016-12-27 17:34:41 +0800

    --测试

---
+++ Haier_Machine/src/wust/dayin1/activity/BindMachineActivity.java
@@ -0,0 +1,16 @@
+package wust.dayin1.activity;
+
+import com.example.haier_machine.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BindMachineActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_bind_machine);
+	}
+}
